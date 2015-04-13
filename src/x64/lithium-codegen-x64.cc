aac51dd3123ef6aa062c3344e57662712c9549ff
Gabriel Southern <gsouther@soe.ucsc.edu>
2015-04-13 12:22:01 -0700

    cleanup merge conflict

--- src/x64/lithium-codegen-x64.cc
+++ src/x64/lithium-codegen-x64.cc
@@ -3051,24 +3051,6 @@ void LCodeGen::DoReturn(LReturn* instr) {
 }
 
 
-/*  TODO: This looks like it was removed.  Test build then delete code
- *  if unneeded
-<<<<<<< HEAD
-void LCodeGen::DoLoadGlobalCell(LLoadGlobalCell* instr) {
-  Register result = ToRegister(instr->result());
-  __ LoadGlobalCell(result, instr->hydrogen()->cell().handle());
-  if (instr->hydrogen()->RequiresHoleCheck()) {
-    __ CompareRoot(result, Heap::kTheHoleValueRootIndex);
-    //TODO: check if this is correct
-    DeoptimizeIfLdSt(equal, instr, Deoptimizer::kHole);
-    //DeoptimizeIfLdSt(equal, instr, "hole");
-  }
-}
-
-
-=======
->>>>>>> master
-*/
 template <class T>
 void LCodeGen::EmitVectorLoadICRegisters(T* instr) {
   DCHECK(FLAG_vector_ics);
@@ -3104,36 +3086,6 @@ void LCodeGen::DoLoadGlobalGeneric(LLoadGlobalGeneric* instr) {
 }
 
 
-<<<<<<< HEAD
-void LCodeGen::DoStoreGlobalCell(LStoreGlobalCell* instr) {
-  Register value = ToRegister(instr->value());
-  Handle<Cell> cell_handle = instr->hydrogen()->cell().handle();
-
-  // If the cell we are storing to contains the hole it could have
-  // been deleted from the property dictionary. In that case, we need
-  // to update the property details in the property dictionary to mark
-  // it as no longer deleted. We deoptimize in that case.
-  if (instr->hydrogen()->RequiresHoleCheck()) {
-    // We have a temp because CompareRoot might clobber kScratchRegister.
-    Register cell = ToRegister(instr->temp());
-    DCHECK(!value.is(cell));
-    __ Move(cell, cell_handle, RelocInfo::CELL);
-    __ CompareRoot(Operand(cell, 0), Heap::kTheHoleValueRootIndex);
-    //DeoptimizeIfLdSt(equal, instr, "hole");
-    DeoptimizeIfLdSt(equal, instr, Deoptimizer::kHole);
-    // Store the value.
-    __ movp(Operand(cell, 0), value);
-  } else {
-    // Store the value.
-    __ Move(kScratchRegister, cell_handle, RelocInfo::CELL);
-    __ movp(Operand(kScratchRegister, 0), value);
-  }
-  // Cells are always rescanned, so no write barrier here.
-}
-
-
-=======
->>>>>>> master
 void LCodeGen::DoLoadContextSlot(LLoadContextSlot* instr) {
   Register context = ToRegister(instr->context());
   Register result = ToRegister(instr->result());
